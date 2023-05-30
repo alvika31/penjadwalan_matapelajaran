ed40ad42093ab9b0c4ba0a5a28c9167e12219eec
Alvika Prahasta <alvikaajiandrianty@gmail.com>
2023-05-30 19:17:08 +0700

    update in admin

--- DB/jadwal_sekolah (1).sql
+++ DB/jadwal_sekolah (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Bulan Mei 2023 pada 13.36
+-- Waktu pembuatan: 30 Bulan Mei 2023 pada 14.16
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.1.12
 
@@ -40,7 +40,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `nama_admin`, `username`, `email`, `password`) VALUES
-(1, 'dakon', 'dakon', 'dakon@gmail.com', 'a8e807b6f5b0aff25aa18b8cdc08f873');
+(1, 'dakon kontolodon', 'dakon', 'dakon@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(2, ' admin dakon', ' admin', ' admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -49,20 +50,26 @@ INSERT INTO `admin` (`id`, `nama_admin`, `username`, `email`, `password`) VALUES
 --
 
 CREATE TABLE `guru` (
-  `id` int(11) NOT NULL,
+  `id_guru` int(11) NOT NULL,
   `nama_guru` varchar(255) NOT NULL,
   `nip` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `jenis_kelamin` varchar(255) NOT NULL
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `jabatan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `guru`
 --
 
-INSERT INTO `guru` (`id`, `nama_guru`, `nip`, `email`, `password`, `jenis_kelamin`) VALUES
-(1, 'komarudin', '12345', 'komarudin@gmail.com', 'f1dd19796fdb3770025a07277bd2dc4f', 'laki-laki');
+INSERT INTO `guru` (`id_guru`, `nama_guru`, `nip`, `email`, `password`, `jenis_kelamin`, `jabatan`) VALUES
+(1, 'Komarudin Firdaus', '12345', 'komarudin@gmail.com', 'f1dd19796fdb3770025a07277bd2dc4f', 'Pria', 'Pengajar'),
+(4, 'Maya Yahya', '8642', 'maya@gmailcom', '0912d0f15f1394268c66639e39b26215', 'Wanita', 'Pengajar'),
+(5, 'Dadang Abdullah', '9832', 'dadang@gmail.com', 'd08726019e4a2a15cb1d49092e4d0522', 'Pria', 'Wali Kelas'),
+(6, 'Nani Suryani', '4509', 'nani@gmail.com', 'f8ea2e8463760785106490befc78c339', 'Wanita', 'Wali Kelas'),
+(8, 'Bagus Subagja Ramadhan', '0897', 'bagus@gmail.com', '9bef81dab3d9d34ba318dd662229d47c', 'Pria', 'Pengajar'),
+(10, 'asd', '32323', 'alvika@hsjjs.hsj', '5f6eb0809f31e88067e51bfd2bb0c50e', 'Pria', 'Pengajar');
 
 -- --------------------------------------------------------
 
@@ -71,9 +78,21 @@ INSERT INTO `guru` (`id`, `nama_guru`, `nip`, `email`, `password`, `jenis_kelami
 --
 
 CREATE TABLE `kelas` (
-  `id` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
   `nama_kelas` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`) VALUES
+(3, 'X RPL 1'),
+(4, 'XI RPL 1'),
+(6, 'XII RPL 1'),
+(7, 'X RPL 2'),
+(8, 'XI RPL 2'),
+(9, 'XII RPL 2');
 
 -- --------------------------------------------------------
 
@@ -82,13 +101,22 @@ CREATE TABLE `kelas` (
 --
 
 CREATE TABLE `mata_pelajaran` (
-  `id` int(11) NOT NULL,
+  `id_mapel` int(11) NOT NULL,
   `nama_pelajaran` varchar(255) NOT NULL,
-  `jadwal` varchar(255) NOT NULL,
+  `jam_awal` time NOT NULL,
+  `jam_selesai` time NOT NULL,
+  `hari` varchar(100) NOT NULL,
   `id_kelas` int(11) NOT NULL,
   `id_ruangan` int(11) NOT NULL,
   `id_guru` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mata_pelajaran`
+--
+
+INSERT INTO `mata_pelajaran` (`id_mapel`, `nama_pelajaran`, `jam_awal`, `jam_selesai`, `hari`, `id_kelas`, `id_ruangan`, `id_guru`) VALUES
+(5, 'Pemograman Web', '17:41:00', '19:41:00', 'senin', 3, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -97,7 +125,7 @@ CREATE TABLE `mata_pelajaran` (
 --
 
 CREATE TABLE `rapot` (
-  `id` int(11) NOT NULL,
+  `id_rapot` int(11) NOT NULL,
   `id_guru` int(11) NOT NULL,
   `id_kelas` int(11) NOT NULL,
   `tanggal` date NOT NULL,
@@ -111,10 +139,21 @@ CREATE TABLE `rapot` (
 --
 
 CREATE TABLE `ruangan` (
-  `id` int(11) NOT NULL,
+  `id_ruangan` int(11) NOT NULL,
   `kode_ruangan` varchar(255) NOT NULL,
   `nama_ruangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `ruangan`
+--
+
+INSERT INTO `ruangan` (`id_ruangan`, `kode_ruangan`, `nama_ruangan`) VALUES
+(1, 'A001', 'Jayusman B11'),
+(3, 'A002', 'Jayusman A1'),
+(4, 'A003', 'Jayusman A2'),
+(5, 'A004', 'Jayusman A3'),
+(6, 'A005', 'Jayusman A4');
 
 -- --------------------------------------------------------
 
@@ -123,20 +162,25 @@ CREATE TABLE `ruangan` (
 --
 
 CREATE TABLE `siswa` (
-  `id` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
   `nama_lengkap` varchar(255) NOT NULL,
   `nis` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `jenis_kelamin` varchar(255) NOT NULL
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `id_kelas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `siswa`
 --
 
-INSERT INTO `siswa` (`id`, `nama_lengkap`, `nis`, `email`, `password`, `jenis_kelamin`) VALUES
-(1, 'alvika', '12345', 'alvika@gmail.com', '11e01fc6fbe051e7074e594784e44519', 'Pria');
+INSERT INTO `siswa` (`id_siswa`, `nama_lengkap`, `nis`, `email`, `password`, `jenis_kelamin`, `id_kelas`) VALUES
+(1, 'alvika', '12345', 'alvika@gmail.com', '11e01fc6fbe051e7074e594784e44519', 'Pria', 3),
+(4, 'raihan sidiq', '1357', 'raihan@gmail.com', 'c26820b8a4c1b3c2aa868d6d57e14a79', 'Pria', 4),
+(6, 'Arip Gusmawan', '8567', 'arip@gmail.com', '6e17a5fd135fcaf4b49f2860c2474c7c', 'Pria', 8),
+(7, 'Novaria Wijaya', '4653', 'nova@gmail.com', 'b151ce4935a3c2807e1dd9963eda16d8', 'Wanita', 4),
+(8, 'Evi Gutamala', '7965', 'evi@gmail.com', 'a440a3d316c5614c7a9310e902f4a43e', 'Wanita', 7);
 
 --
 -- Indexes for dumped tables
@@ -152,37 +196,37 @@ ALTER TABLE `admin`
 -- Indeks untuk tabel `guru`
 --
 ALTER TABLE `guru`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_guru`);
 
 --
 -- Indeks untuk tabel `kelas`
 --
 ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_kelas`);
 
 --
 -- Indeks untuk tabel `mata_pelajaran`
 --
 ALTER TABLE `mata_pelajaran`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_mapel`);
 
 --
 -- Indeks untuk tabel `rapot`
 --
 ALTER TABLE `rapot`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_rapot`);
 
 --
 -- Indeks untuk tabel `ruangan`
 --
 ALTER TABLE `ruangan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_ruangan`);
 
 --
 -- Indeks untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_siswa`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -192,43 +236,43 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `guru`
 --
 ALTER TABLE `guru`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT untuk tabel `mata_pelajaran`
 --
 ALTER TABLE `mata_pelajaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `rapot`
 --
 ALTER TABLE `rapot`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_rapot` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `ruangan`
 --
 ALTER TABLE `ruangan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ruangan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
